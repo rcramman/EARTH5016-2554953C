3977a3bde696ff2576bb42112c50e49915a60131
Rebecca <2554953C@student.gla.ac.uk>
2026-01-19 13:01:24 +0000

    implemented the 2nd-order Runge-Kutta scheme

--- usr/run_advdiff1D.m
+++ usr/run_advdiff1D.m
@@ -17,7 +17,7 @@ u0    = 1e-6;          % advection speed [m/s]
 
 BC    = 'periodic';    % boundary condition option flag ('insulating', 'periodic')
 ADVN  = 'UPW3';        % advection scheme ('UPW1', 'CFD2', 'UPW3')
-TINT  = 'FE1';         % time integration scheme ('FE1', 'RK2')
+TINT  = 'RK2';         % time integration scheme ('FE1', 'RK2')
 
 yr    = 3600*24*365;   % seconds per year [s]
 tend  = W/max(u0,k0)/4;  % stopping time [s]
