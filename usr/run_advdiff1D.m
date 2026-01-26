37f1586770c5b29a85c2683158af994721c906ec
Rebecca <2554953C@student.gla.ac.uk>
2026-01-26 10:51:07 +0000

    introduced new variables for session 5

--- usr/run_advdiff1D.m
+++ usr/run_advdiff1D.m
@@ -4,9 +4,19 @@
 clear; close all;
 
 % set model parameters
-W     = 1000;          % domain width [m]
-N     = 200;           % grid size
+W     = le3;           % domain width [m]
+N     = le3;           % grid size
 dx    = W/N;           % grid spacing
+Nz    = 100;           % grid size z-direction
+Nx    = Nz*W/D;        % grid size x-direction
+h     = D/Nz;          % grid spacing (h = dx = dz)
+
+kT0   = 2;             % thermal conductivty [W/m/K]
+rho0  = 2700;          % density [kg/m3]
+cP0   = 1100;          % heat capacity [J/kg/K]
+Qr0   = le-6;          % heat productivity [W/m3]
+u0    = le-6;          % advection x-speed [m/s]
+w0    = le-6;          % advection z-speed [m/s]
 
 T0    = 100;           % initial background temperature [C]
 dT    = 1000;          % initial temperature peak amplitude [C]
